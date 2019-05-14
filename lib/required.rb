25ea8c1d1e9e157aa6f8ccf4b9000202e1e82dc8
Toni-Ty <antonique2479@yahoo.com>
2019-05-14 00:47:47 +0000

    updated files and folders

---
+++ lib/required.rb
@@ -0,0 +1,7 @@
+require 'nokogiri'  #add to spec as well
+require 'pry'
+require 'open-uri'
+
+require_relative "./required/version"
+require_relative './required/cli'
+require_relative './required/Virtual_Cafe' 
