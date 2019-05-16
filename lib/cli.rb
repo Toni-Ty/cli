deffa2905e82196d30dbb2846a1e357defc053b2
Toni-Ty <antonique2479@yahoo.com>
2019-05-16 02:03:50 +0000

    updated files

--- lib/cli.rb
+++
@@ -1,6 +0,0 @@
-require "cli/version"
-
-module Cli
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/required.rb
+++ lib/required.rb
@@ -1,3 +1,11 @@
+require "cli/version"
+
+module Cli
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+
 require 'nokogiri'  #add to spec as well
 require 'pry'
 require 'open-uri'
