d3b91e55f96d9895ee91311041272c14d82810ae
Toni-Ty <antonique2479@yahoo.com>
2019-05-18 05:46:04 +0000

    minor updates

--- lib/cli_application/cli_scraper.rb
+++ lib/cli_application/cli_scraper.rb
@@ -4,34 +4,35 @@ class VirtualCafe::CLI_SCRAPER
 
 
 
-def self.srape_coffee_description
-	self.scrape_drinks
-end
+# def self.srape_coffee_description
+# 	self.scrape_drinks
+# end
 
 
-def self.scrape_drinks
-	drinks = []
+# def self.scrape_drinks
+# 	drinks = []
 
-drinks << self.scrape_latteart
+# drinks << self.scrape_latteart
 
-#coffee_drink = self.new
-#coffee_drink.name = Espresso
-#coffee_drink.coffee_description = " "
-#coffee_drink.coffee_ingredients = " "
+# #coffee_drink = self.new
+# #coffee_drink.name = Espresso
+# #coffee_drink.coffee_description = " "
+# #coffee_drink.coffee_ingredients = " "
 
 
-end
+# end
 
-def self.scrape_latteart
+# def self.scrape_latteart
 
-doc = Nokogiri::HTML(open("https://www.latteartguide.com/2016/01/different-types-of-coffee.html#piccolo"))
-doc.css("div.awr-i").each do |drinks|
-      drinks.css('//p[9]').collect do |drink|
-        espresso_drink_ingredients = 
+# doc = Nokogiri::HTML(open("https://www.latteartguide.com/2016/01/different-types-of-coffee.html#piccolo"))
+# doc.css("div.awr-i").each do |drinks|
+#       drinks.css('//p[9]').collect do |drink|
+#         espresso_drink_ingredients = 
         
-#describes how to make the coffee drink
-def self.scrape_coffee_ingredients
+# #describes how to make the coffee drink
+# def self.scrape_coffee_ingredients
 	
 	
 	
+# end
 end
--- lib/cli_application/virtual_cafe_drinks.rb
+++ lib/cli_application/virtual_cafe_drinks.rb
@@ -1,20 +1,20 @@
 class VirtualCafe::Virtual_Cafe_Drinks
 
-attr_accessor :name, :coffee_description, :coffee_ingredients
+# attr_accessor :name, :coffee_description, :coffee_ingredients
 
-@@all = []
-
-
-def initialize (name = nil)
-	@name = name
-	@all << self
-	end
-end
+# @@all = []
 
 
-def self.all
-	@all
-  end
+# def initialize (name = nil)
+# 	@name = name
+# 	@all << self
+# 	end
+# end
+
+
+# def self.all
+# 	@all
+#   end
 end
 
 
