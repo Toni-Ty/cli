6b787234a216d555d8c5d7c5cdf96e363ffa3c10
Toni-Ty <antonique2479@yahoo.com>
2019-05-20 03:44:18 +0000

    changed line-endings in bin file to LF

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -1,10 +1,10 @@
-class VirtualCafe::CLI 
+class VirtualCafe::CLI
 #encapsulate all of my logic into one #object/CLI controller
-def hello 
-  puts "hello world"
+def hello
+  puts "hello w."
 end
 
-# def call 
+# def call
 # 	#list_drinks
 # 	#main_menu
 # 	#coffee_drink_ingredients
@@ -14,19 +14,19 @@ end
 
 # 	puts "Hello, and Welcome to the Virtual Coffee Cafe!"
 # 	puts "Today we will learn about espresso based coffee drinks!"
-# 	puts #espresso scraped data here 	
-	
+# 	puts #espresso scraped data here
+
 # 	@coffee_drink = VirtualCafe::VirtualCafeDrinks.coffee_description
 # 	@coffee_drink.each.with_index(1) do |coffee, index|
-	
+
 # 	puts "#{index}. #{coffee_drink.name} - #{@coffee_drink.description}
 # 	end
 # end
 
-# 	puts "Which coffee drink would you like to learn about?"  
-	
+# 	puts "Which coffee drink would you like to learn about?"
 
- 
+
+
 # def main_menu
 # 	input = nil
 # 	while input != "exit"
@@ -35,7 +35,7 @@ end
 # 	if input.to_i > 0 && if input.to_i < 12;
 # 		selected_coffee_drink = @coffee_drink[input.to_i-1]
 # 		puts "#{selected_coffee_drink.name} - #{@selected_coffee_drink.description}
-		
+
 # 		puts "Would you like the ingredients for this drink [enter y/n]? Would you like to start over [enter start over]?
 
 # 		input = gets.strip.downcase
@@ -43,9 +43,9 @@ end
 # 			selected_coffee_drink
 # 			puts "#{selected_coffee_drink.coffee_ingredients}
 # 			list_drinks
-# 		elsif input == "start over" 
+# 		elsif input == "start over"
 # 		main_menu
-# 		elsif input == "n" 
+# 		elsif input == "n"
 # 			puts "Thank you, I hope you had fun!  Have a wonderful day!
 # 			exit
 # 		else
@@ -59,15 +59,9 @@ end
 # 	selected_coffee_drink
 # 	puts "#{selected_coffee_drink.coffee_ingredients}"
 # end
-	
-	
+
+
 # def exit
 
 # end
 end
-	
-	
-	
-
-	
-
