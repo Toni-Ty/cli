61c5a01d552f3be060d95a001515ef18b582c78c
Toni-Ty <antonique2479@yahoo.com>
2019-05-23 06:14:22 +0000

    added pry and edited scraper

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -2,58 +2,58 @@ class VirtualCafe::CLI_INTERFACE
 #encapsulate all of my logic into one #object/CLI controller
 
 
-def call
-	list_drinks
-	main_menu
-	coffee_drink_ingredients
-end
+# def call
+# 	list_drinks
+# 	main_menu
+# 	coffee_drink_ingredients
+# end
 
-def list_drinks
-
-	puts "Hello, and Welcome to the Virtual Coffee Cafe!"
-	puts "Today we will learn about espresso based coffee drinks!"
-  puts "Which coffee drink would you like to learn about?"
-	# puts espresso scraped data here
-
-	@coffee_drink = VirtualCafe::VirtualCafeDrinks.coffee_description
-	@coffee_drink.each.with_index(1) do |coffee, index|
-
-	puts "#{index}. #{coffee_drink.name} - #{@coffee_drink.description}
-
-  end
-
-
-def main_menu
-puts "Please select a number from 1-12"
-	input = nil
-	if input.to_i > 0 && if input.to_i < 12;
-		selected_coffee_drink = @coffee_drink[input.to_i-1]
-		 puts "#{selected_coffee_drink.name} - #{@selected_coffee_drink.description}"
-
-		puts "Would you like the ingredients for this drink [enter y/n]? Would you like to start over [enter start over]?
-
-		input = gets.strip.downcase
-		if input == "y"
-			selected_coffee_drink
-			#  {selected_coffee_drink.coffee_ingredients}
-			list_drinks
-		elsif input == "start over"
-		main_menu
-		elsif input == "n"
-			puts "Thank you, I hope you had fun!  Have a wonderful day!
-			exit
-		else
-			puts "Sorry, that's an invalid response."
-		main_menu
-		end
-	end
-end
-
- def coffee_drink_ingredients()
- 	# selected_coffee_drink
-	# puts "#{selected_coffee_drink.coffee_ingredients}"
-end
-
+# def list_drinks
+#
+# 	puts "Hello, and Welcome to the Virtual Coffee Cafe!"
+# 	puts "Today we will learn about espresso based coffee drinks!"
+#   puts "Which coffee drink would you like to learn about?"
+# 	# puts espresso scraped data here
+#
+# 	@coffee_drink = VirtualCafe::VirtualCafeDrinks.coffee_description
+# 	@coffee_drink.each.with_index(1) do |coffee, index|
+#
+# 	puts "#{index}. #{coffee_drink.name} - #{@coffee_drink.description}
+#
+#   end
+#
+#
+# def main_menu
+# puts "Please select a number from 1-12"
+# 	input = nil
+# 	if input.to_i > 0 && if input.to_i < 12;
+# 		selected_coffee_drink = @coffee_drink[input.to_i-1]
+# 		 puts "#{selected_coffee_drink.name} - #{@selected_coffee_drink.description}"
+#
+# 		puts "Would you like the ingredients for this drink [enter y/n]? Would you like to start over [enter start over]?
+#
+# 		input = gets.strip.downcase
+# 		if input == "y"
+# 			selected_coffee_drink
+# 			#  {selected_coffee_drink.coffee_ingredients}
+# 			list_drinks
+# 		elsif input == "start over"
+# 		main_menu
+# 		elsif input == "n"
+# 			puts "Thank you, I hope you had fun!  Have a wonderful day!
+# 			exit
+# 		else
+# 			puts "Sorry, that's an invalid response."
+# 		main_menu
+# 		end
+# 	end
+# end
+#
+#  def coffee_drink_ingredients()
+#  	# selected_coffee_drink
+# 	# puts "#{selected_coffee_drink.coffee_ingredients}"
+# end
+#
 
 def exit
 
--- lib/cli_application/cli_scraper.rb
+++ lib/cli_application/cli_scraper.rb
@@ -1,38 +1,39 @@
+require 'pry'
 #should return instances of coffee_descriptions
 #info below will be scraped
 class VirtualCafe::CLI_SCRAPER
 
 
 
-def self.srape_coffee_description
-	self.scrape_drinks
-end
-
-
-# def self.scrape_drinks
-# 	drinks = []
-#
-# drinks << self.scrape_latteart
-#
-# #coffee_drink = self.new
-# #coffee_drink.name = Espresso
-# #coffee_drink.coffee_description = " "
-# #coffee_drink.coffee_ingredients = " "
-#
-#
+# def self.srape_coffee_description
+# 	self.scrape_drinks
 # end
+
+
 
 def self.scrape_latteart
 
+
 doc = Nokogiri::HTML(open("https://www.latteartguide.com/2016/01/different-types-of-coffee.html#piccolo"))
 doc.css("div.awr-i").each do |drinks|
-      drinks.css('//p[9]').collect do |drink|
-        espresso_drink_ingredients =
+		coffee_blurb = drinks.css("p").text
+		binding.pry
+			coffee_blurb.each.with_index(1) do |coffee, index|
+
+		end
+	end
+end
+
+
+
+end
+
+
+
+
+
+        # espresso_drink_ingredients
+
 
 #describes how to make the coffee drink
-def self.scrape_coffee_ingredients
-
-
-
-end
-end
+# def self.scrape_coffee_ingredients
