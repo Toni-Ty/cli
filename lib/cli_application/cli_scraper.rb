b85e88e68ec441b223b0c68c1aeba0ec729640d9
Toni-Ty <antonique2479@yahoo.com>
2019-05-29 04:55:36 +0000

    made changes to scraping method

--- lib/cli_application/cli_scraper.rb
+++ lib/cli_application/cli_scraper.rb
@@ -12,16 +12,19 @@ def scrape_cafe
 	coffee = page.css("div.col-md-9.list section")
 
 	coffee.each do |coffee_drink|
+		drinks = []
 	  coffee_descrip = coffee_drink.css("h2").text
-		puts coffee_descrip
+		drinks << coffee_descrip
+	 	drinks.each_with_index { |c, index| puts "#{index+1}. #{c}"}
+
 		# 	@coffee_description = VirtualCafe::Virtual_Cafe_Drinks
 		# 	 	@coffee_description.each_with_index(1) do |coffee_descrip, index|
 		#
 		# 	 	puts "#{index}. #{coffee_descrip}"
 	  #   end
-		# end
+		end
 	end
-end
+
 					# @coffee_drink = VirtualCafe::VirtualCafeDrinks.coffee_description
 					# @coffee_drink.each.with_index(1) do |coffee, index|
 					# puts "#{index}. #{coffee_drink.name} - #{@coffee_drink.description}
