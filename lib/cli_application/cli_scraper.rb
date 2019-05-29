9c44516aeb6731b3f41422089aa09ffc4ba7abcd
Toni-Ty <antonique2479@yahoo.com>
2019-05-29 00:54:54 +0000

    made changes to scraping method

--- lib/cli_application/cli_scraper.rb
+++ lib/cli_application/cli_scraper.rb
@@ -5,12 +5,19 @@ class VirtualCafe::CLI_SCRAPER
 
 # coffee_description
 
-def self.scrape_latteart
-doc = Nokogiri::HTML(open("http://www.cafepoint.co.uk/different-types-of-coffee/"))
-doc.css("div.awr-i").each do |drinks|
-		coffee_blurb = drinks.css("p").text
-		binding.pry
-			coffee_blurb.each_with_index(1) do |coffee, index|
+def self.scrape_cafe
+
+	site = "http://www.cafepoint.co.uk/different-types-of-coffee/"
+
+	page = Nokogiri::HTML(open(site))
+	coffee = page.css("div.col-md-9.list section")
+
+	coffee.each do |coffee_drink|
+	  coffee_descrip = coffee_drink.css("h2").text
+	   coffee_descrip.each_with_index(1) do |coffee, index|
+	    end
+	end
+
 				# 	@coffee_drink = VirtualCafe::VirtualCafeDrinks.coffee_description
 				# 	@coffee_drink.each.with_index(1) do |coffee, index|
 				#
