29bf96912ffd57d75f6225461362a88e8c23be54
Toni-Ty <antonique2479@yahoo.com>
2019-06-22 05:37:57 +0000

    added gsub to recipes in scraper

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -30,9 +30,12 @@ end
 
 def validate(input)
 		drink_selection = VirtualCafe::Virtual_Cafe_Drinks.find_by_id(input)
-		drink_selection ?  : invalid_input
+		drink_selection ? coffee_description : invalid_input
 end
 
+def invalid_input
+	puts "I don't understand, please try again".magenta
+end
 
 def print_coffees
 	VirtualCafe::CLI_SCRAPER.scrape_cafe
@@ -53,7 +56,7 @@ def continue_on_to_recipe
 	puts "Would you like the recipe for this drink?".light_blue
 	puts "If so, please type [the name of the drink]...Or type [start over]".light_blue
 	puts "If you'd like to exit, please type [exit]".light_blue
-	coffee_recipes
+
 end
 
 
