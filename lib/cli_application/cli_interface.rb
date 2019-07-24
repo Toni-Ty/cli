5b2a7ff42e08b69bcd6690a3fa794464b5171d8b
Toni-Ty <antonique2479@yahoo.com>
2019-07-24 22:44:03 +0000

    refactored @input to include downcase method

--- lib/cli_application/cli_interface.rb
+++ lib/cli_application/cli_interface.rb
@@ -1,69 +1,69 @@
 # User interface and menu
 class VirtualCafe::CLI_INTERFACE
 
-def call
-		puts "Hello, and Welcome to the Virtual Coffee Cafe!".magenta
-		puts "Today we will learn about espresso based coffee drinks!".magenta
-	  puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ".blue
-		while @input != "exit"
-		print_coffees
-		gets_user_input
-		valid_coffee_input ? show_drink : invalid_response
-		continue_on_to_new_recipe?
-	end
-end
-
-def print_coffees
-	VirtualCafe::CLI_SCRAPER.scrape_cafe
-	@create_coffee =  VirtualCafe::Virtual_Cafe_Drinks.all
-  puts "Which coffee drink would you like to learn about?".magenta
-  puts "Please select a number from 1-10".magenta
-	puts "(for example, FRAPPUCCINO = 1, CARAMEL MACCHIATO = 2, etc...)".blue
-  @create_coffee[0..14].each.with_index  { |drink, i| puts "#{i + 1}. #{drink.name}"}
-	puts ""
-end
-
-
-def gets_user_input
-	@input = gets.strip.downcase
-	if @input.to_i < 16
-			valid_coffee_input
-	else
-		invalid_response
- end
-end
-
-def valid_coffee_input
-	VirtualCafe::Virtual_Cafe_Drinks.exist?(@input)
-end
-
-def invalid_response
-	puts "Sorry that's an invalid response. Please try again :)".magenta
-	puts ""
-	call
-end
-
-
-def show_drink
-	drink_selection = VirtualCafe::Virtual_Cafe_Drinks.find_by_id(@input)
-	puts "#{drink_selection.name}"
-	puts "#{drink_selection.recipe}"
-end
-
-
-def continue_on_to_new_recipe?
-	VirtualCafe::CLI_SCRAPER.scrape_cafe
-	puts ""
-	puts "Would you like to see a new coffee drink and recipe?".light_blue
-	puts "If so, please type [Y]...Or type [exit] to leave the cafe".light_blue
-	gets_user_input
-	if @input == "Y"
-		call
-	elsif @input == "exit"
-		puts "Have a great day and thank you for using the Virtual Cafe!".magenta
-		exit
-	else
-		invalid_response
-			end
+	def call
+			puts "Hello, and Welcome to the Virtual Coffee Cafe!".magenta
+			puts "Today we will learn about espresso based coffee drinks!".magenta
+		  puts "      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ".blue
+			while @input != "exit"
+			print_coffees
+			gets_user_input
+			valid_coffee_input ? show_drink : invalid_response
+			continue_on_to_new_recipe?
 		end
 	end
+
+	def print_coffees
+		VirtualCafe::CLI_SCRAPER.scrape_cafe
+		@create_coffee =  VirtualCafe::Virtual_Cafe_Drinks.all
+	  puts "Which coffee drink would you like to learn about?".magenta
+	  puts "Please select a number from 1-10".magenta
+		puts "(for example, FRAPPUCCINO = 1, CARAMEL MACCHIATO = 2, etc...)".blue
+	  @create_coffee[0..14].each.with_index  { |drink, i| puts "#{i + 1}. #{drink.name}"}
+		puts ""
+	end
+
+
+	def gets_user_input
+		@input = gets.strip.downcase
+		if @input.to_i < 16
+				valid_coffee_input
+		else
+			invalid_response
+	 end
+	end
+
+	def valid_coffee_input
+		VirtualCafe::Virtual_Cafe_Drinks.exist?(@input)
+	end
+
+	def invalid_response
+		puts "Sorry that's an invalid response. Please try again :)".magenta
+		puts ""
+		call
+	end
+
+
+	def show_drink
+		drink_selection = VirtualCafe::Virtual_Cafe_Drinks.find_by_id(@input)
+		puts "#{drink_selection.name}"
+		puts "#{drink_selection.recipe}"
+	end
+
+
+	def continue_on_to_new_recipe?
+		VirtualCafe::CLI_SCRAPER.scrape_cafe
+		puts ""
+		puts "Would you like to see a new coffee drink and recipe?".light_blue
+		puts "If so, please type [Y]...Or type [exit] to leave the cafe".light_blue
+		gets_user_input
+		if @input == "Y"
+			call
+		elsif @input == "exit"
+			puts "Have a great day and thank you for using the Virtual Cafe!".magenta
+			exit
+		else
+			invalid_response
+				end
+			end
+		end
--- lib/cli_application/cli_scraper.rb
+++ lib/cli_application/cli_scraper.rb
@@ -2,16 +2,16 @@
 
 class VirtualCafe::CLI_SCRAPER
 
-def self.scrape_cafe
+	def self.scrape_cafe
 
-	site = "http://www.cafepoint.co.uk/different-types-of-coffee/"
+		site = "http://www.cafepoint.co.uk/different-types-of-coffee/"
 
-	page = Nokogiri::HTML(open(site))
-		coffee = page.css("div.col-md-9.list section")
-		coffee.each do |drink|
-		name = drink.css('h2').text
-		recipe = drink.css('li').text.gsub(/.Step/, ". Step")
-		VirtualCafe::Virtual_Cafe_Drinks.new(name, recipe)
+		page = Nokogiri::HTML(open(site))
+			coffee = page.css("div.col-md-9.list section")
+			coffee.each do |drink|
+			name = drink.css('h2').text
+			recipe = drink.css('li').text.gsub(/.Step/, ". Step")
+			VirtualCafe::Virtual_Cafe_Drinks.new(name, recipe)
+			end
 		end
 	end
-end
